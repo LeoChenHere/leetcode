96cc6cce7ccf687f1cbc44291765db5b7871cb77
Leo Chen <leo@leochen.i234.me>
2017-05-22 22:38:48 +0800

    .gitignore is now working

---
+++ src/main/java/leetcode/MaxSubArray.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetcode;
+
+/**
+ *
+ * @author leo
+ */
+public class MaxSubArray {
+    public static void main(String[] args){
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        (new MaxSubArray()).maxSubArray(nums);
+    }
+    
+    public int maxSubArray(int[] nums) {
+        int result = 0;
+        
+        int startIndex = 0;
+        int endIndex = 0;
+        while( startIndex < ){
+            
+        }
+        
+        return result;
+    }
+    
+    int sumArray(int[] nums){
+        for(){
+        }
+    }
+}
